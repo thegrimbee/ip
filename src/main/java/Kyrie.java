78376f8c73e568416a5891f7e5b04b5facd7aa9c
thegrimbee <gabriel.mario.antaputra@gmail.com>
2025-01-24 10:02:12 +0800

    Changed name to kyrie and added welcome and goodbye message

---
+++ src/main/java/Kyrie.java
@@ -0,0 +1,8 @@
+public class Kyrie {
+    public static void main(String[] args) {
+        String logo = "Kyrie";
+        String separator = "\n____________________________________________________________";
+        System.out.println("Shalom aleikhim, I am " + logo +"\nTell me what you desire" + separator);
+        System.out.println("Farewell, my friend!" + separator);
+    }
+}
