6632c83b39cbcb419e5480eaf58b0d1b54949e37
thegrimbee <gabriel.mario.antaputra@gmail.com>
2025-01-24 10:34:46 +0800

    added tasks and list command

--- src/main/java/Kyrie.java
+++ src/main/java/Kyrie.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
+
 public class Kyrie {
     public static void main(String[] args) {
         String logo = "Kyrie";
-        String separator = "\n____________________________________________________________";
+        String separator = "\n\t____________________________________________________________\n\t";
         System.out.println("Shalom aleikhim, I am " + logo +"\nTell me what you desire" + separator);
-        System.out.println("Farewell, my friend!" + separator);
+        Scanner sc = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
+        String input;
+        while (true) {
+            input = sc.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else if (input.equals("list")) {
+                System.out.println(separator + "Here are the tasks in your list:");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("\t" + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(separator);
+            } else {
+                tasks[taskCount++] = input;
+                System.out.println(separator + "added a new task: " + input + separator);
+            }
+        }
+        System.out.println(separator + "Farewell, my friend!" + separator);
+        sc.close();
     }
 }
