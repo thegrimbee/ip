8608d03cdb46e167bb45356f867147d4fb98e5e0
thegrimbee <gabriel.mario.antaputra@gmail.com>
2025-01-24 12:02:35 +0800

    Made Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+    private Boolean isDone;
+    private String name;
+
+    public Task(String name) {
+        this.name = name;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void unmarkAsDone() {
+        this.isDone = false;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Boolean isDone() {
+        return this.isDone;
+    }
+
+    @Override
+    public String toString() {
+        return this.isDone ? "[X] " + this.name : "[ ] " + this.name;
+    }
+}
