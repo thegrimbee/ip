5184d36ba25c7b82db60dca42cad0c97752f62a8
thegrimbee <gabriel.mario.antaputra@gmail.com>
2025-01-26 15:36:06 +0800

    Add echo

--- src/main/java/Kyrie.java
+++ src/main/java/Kyrie.java
@@ -4,5 +4,14 @@ public class Kyrie {
         String separator = "\n____________________________________________________________";
         System.out.println("Shalom aleikhim, I am " + logo +"\nTell me what you desire" + separator);
         System.out.println("Farewell, my friend!" + separator);
+        Scanner sc = new Scanner(System.in);
+        String input;
+        while (true) {
+            input = sc.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(separator + '\n' + input);
+        }
     }
 }
